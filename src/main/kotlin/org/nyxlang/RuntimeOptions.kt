703106ca55d1e97a108a3fd28bfd9c3f7bab4446
Daniel Kleebinder <daniel.kleebinder@gmail.com>
2021-07-25 13:58:41 +0200

    Implement simple coroutines

--- src/main/kotlin/org/nyxlang/RuntimeOptions.kt
+++ src/main/kotlin/org/nyxlang/RuntimeOptions.kt
@@ -1,5 +1,7 @@
 package org.nyxlang
 
+import java.util.concurrent.Executors
+
 /**
  * Contains options that can be enabled or disabled
  * for the runtime environment.
@@ -10,4 +12,14 @@ object RuntimeOptions {
      * Allows for printing additional debug information.
      */
     var debug: Boolean = false
+
+    /**
+     * The number of processor cores available on the system.
+     */
+    val processorCores = Runtime.getRuntime().availableProcessors()
+
+    /**
+     * The global programming language thread pool.
+     */
+    val threadPool = Executors.newFixedThreadPool(processorCores)
 }
--- src/main/kotlin/org/nyxlang/interpreter/visitor/AsyncVisitor.kt
+++ src/main/kotlin/org/nyxlang/interpreter/visitor/AsyncVisitor.kt
@@ -1,7 +1,9 @@
 package org.nyxlang.interpreter.visitor
 
+import org.nyxlang.RuntimeOptions
 import org.nyxlang.interpreter.IInterpreter
 import org.nyxlang.interpreter.result.IRuntimeResult
+import org.nyxlang.interpreter.result.emptyResult
 import org.nyxlang.parser.ast.AsyncNode
 import org.nyxlang.parser.ast.INode
 
@@ -11,6 +13,7 @@ import org.nyxlang.parser.ast.INode
 class AsyncVisitor : IVisitor {
     override fun visit(interpreter: IInterpreter, node: INode): IRuntimeResult {
         val asyncNode = node as AsyncNode
-        return interpreter.interpret(asyncNode.statement)
+        RuntimeOptions.threadPool.submit { interpreter.interpret(asyncNode.statement) }
+        return emptyResult()
     }
 }
