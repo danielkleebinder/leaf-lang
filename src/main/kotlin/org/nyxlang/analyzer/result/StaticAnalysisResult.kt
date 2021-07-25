e2ab508ebaa6ba8cb6f2b594a4799af2469914f6
Daniel Kleebinder <daniel.kleebinder@gmail.com>
2021-07-25 14:39:54 +0200

    Add static analysis result

---
+++ src/main/kotlin/org/nyxlang/analyzer/result/StaticAnalysisResult.kt
@@ -0,0 +1,7 @@
+package org.nyxlang.analyzer.result
+
+/**
+ * Static analysis result is returned by the analytical visitors
+ * to check for certain properties.
+ */
+class StaticAnalysisResult(val type: String)
