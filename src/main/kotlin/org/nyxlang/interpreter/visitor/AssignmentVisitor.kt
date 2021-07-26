34ca3f1267a235ed7c3e88a41b339dc0a19e1275
Daniel Kleebinder <daniel.kleebinder@gmail.com>
2021-07-26 20:21:32 +0200

    Implement assignment visitor set call

--- src/main/kotlin/org/nyxlang/interpreter/visitor/AssignmentVisitor.kt
+++ src/main/kotlin/org/nyxlang/interpreter/visitor/AssignmentVisitor.kt
@@ -1,10 +1,11 @@
 package org.nyxlang.interpreter.visitor
 
 import org.nyxlang.interpreter.IInterpreter
+import org.nyxlang.interpreter.exception.VisitorException
 import org.nyxlang.interpreter.result.IRuntimeResult
 import org.nyxlang.interpreter.result.emptyResult
-import org.nyxlang.parser.ast.INode
 import org.nyxlang.parser.ast.AssignmentNode
+import org.nyxlang.parser.ast.INode
 
 /**
  * Interprets the var assign node.
@@ -12,10 +13,22 @@ import org.nyxlang.parser.ast.AssignmentNode
 class AssignmentVisitor : IVisitor {
     override fun visit(interpreter: IInterpreter, node: INode): IRuntimeResult {
         val assignmentNode = node as AssignmentNode
-        val varName = assignmentNode.name
-        val varAssignment = assignmentNode.assignmentExpr
+        val name = assignmentNode.name
+        val offsetExpr = assignmentNode.offsetExpr
+        val assignmentExpr = assignmentNode.assignmentExpr
         val activationRecord = interpreter.activationRecord!!
-        activationRecord[varName] = interpreter.interpret(varAssignment).data
+
+        val assignmentValue = interpreter.interpret(assignmentExpr).data
+        if (offsetExpr != null) {
+            val offset = interpreter.interpret(offsetExpr).data
+                    ?: throw VisitorException("Provided get offset $offsetExpr is invalid")
+            val storedValue = activationRecord[name]
+                    ?: throw VisitorException("Variable with name \"$name\" does not exist")
+            storedValue.set(offset, assignmentValue!!)
+        } else {
+            activationRecord[name] = assignmentValue
+        }
+
         return emptyResult()
     }
 }
